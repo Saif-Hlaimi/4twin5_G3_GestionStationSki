222627d4f0a7ac4c61fe010a1bdc3ed0d1e4c971
ferielyahyaoui <104622808+ferielyahyaoui@users.noreply.github.com>
2025-04-16 21:29:53 +0100

    Updating

--- src/main/java/tn/esprit/spring/controllers/RegistrationRestController.java
+++ src/main/java/tn/esprit/spring/controllers/RegistrationRestController.java
@@ -15,6 +15,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RegistrationRestController {
     private final IRegistrationServices registrationServices;
+    @Operation(description = "Add Registration ")
+    @PostMapping("/add")
+    public Registration addRegistration(@RequestBody Registration registration){
+        return  registrationServices.addRegistration(registration);
+    }
+
+    @Operation(description = "Retrieve registrations all")
+    @GetMapping("/getall")
+    public List<Registration> retrieveAllRegistration(){
+        return  registrationServices.retrieveAllRegistration();
+    }
     
     @Operation(description = "Add Registration and Assign to Skier")
     @PutMapping("/addAndAssignToSkier/{numSkieur}")
